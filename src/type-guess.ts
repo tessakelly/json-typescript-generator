3d6e3271394665a4ecebc1f83e1915870dd64079
Tessa Kelly <6866749+tessakelly@users.noreply.github.com>
2019-08-30 20:22:38 -0500

    Handle optional properties of objects

--- src/type-guess.ts
+++ src/type-guess.ts
@@ -3,6 +3,7 @@ interface JsonType {
   number?: boolean;
   string?: boolean;
   null?: boolean;
+  undefined?: boolean;
   object?: string; // key in knownObjectTypes
   array?: number; // key in knownTypes
 }
@@ -20,11 +21,18 @@ export default function genTypes(input: string) {
     [key:string]: number;
   } = {};
 
+  function makeValidIdentifier(name: string) {
+    const identifier = name.replace(/^[^A-za-z]+|\W+/gi, '');
+    return identifier.length ?
+      identifier.charAt(0).toUpperCase() + identifier.slice(1) :
+      'Type';
+  }
+
   function generateNewTypeName(propertyName: string) {
-    let typeName = propertyName.charAt(0).toUpperCase() + propertyName.slice(1);
+    let typeName = makeValidIdentifier(propertyName);
     if (knownObjectTypes[typeName]) {
       if (!usedTypeNames[typeName]) {
-        usedTypeNames[typeName] = 1;
+        usedTypeNames[typeName] = 2;
       }
       while (knownObjectTypes[`${typeName}_${usedTypeNames[typeName]}`]) {
         usedTypeNames[typeName] += 1;
@@ -35,18 +43,43 @@ export default function genTypes(input: string) {
     return typeName;
   }
 
+  function addPropertyToTypeIfUndefined(propName: string, type: JsonObjectType) {
+    if (!type[propName]) {
+      const childTypeIndex = knownTypes.length;
+      knownTypes.push({});
+      type[propName] = childTypeIndex;
+    }
+  }
+
+  function checkForUndefinedTypes(obj: any, type: JsonObjectType) {
+    const allKeys = {};
+    for (const key of Object.keys(obj)) {
+      allKeys[key] = false;
+    }
+    for (const key of Object.keys(type)) {
+      if (allKeys[key] === undefined) {
+        allKeys[key] = false;
+      } else {
+        allKeys[key] = true;
+      }
+    }
+    const optionalProperties = Object.keys(allKeys).filter((key) => !allKeys[key]);
+    optionalProperties.forEach((key) => {
+      addPropertyToTypeIfUndefined(key, type);
+      knownTypes[type[key]].undefined = true;
+    });
+  }
+
   function processObject(obj: any, type: JsonType, name?: string) {
     if (!type.object) {
       type.object = generateNewTypeName(name || 'Type');
       knownObjectTypes[type.object] = {};
+    } else {
+      checkForUndefinedTypes(obj, knownObjectTypes[type.object]);
     }
     for (const key of Object.keys(obj)) {
       const objectType = knownObjectTypes[type.object];
-      if (!objectType[key]) {
-        const childTypeIndex = knownTypes.length;
-        knownTypes.push({});
-        objectType[key] = childTypeIndex;
-      }
+      addPropertyToTypeIfUndefined(key, objectType);
       processNode(obj[key], knownTypes[objectType[key]], key);
     }
   }
@@ -111,7 +144,7 @@ export default function genTypes(input: string) {
       const type = knownObjectTypes[typeName];
       result += `export interface ${typeName} {\n`;
       for (const prop of Object.keys(type)) {
-        result += `  ${prop}: ${printPropertyType(knownTypes[type[prop]])};\n`;
+        result += `  ${prop}${knownTypes[type[prop]].undefined ? '?' : ''}: ${printPropertyType(knownTypes[type[prop]])};\n`;
       }
       result += '}\n\n';
     }
